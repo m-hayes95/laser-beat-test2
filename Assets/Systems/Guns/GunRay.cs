996bfb00bee1094d05e65c9f9078f99ec09ea6ef
m-hayes95 <michaelhayes322@gmail.com>
2023-10-14 21:17:49 +0100

    Added gun shooting functionality
    using raycasts

---
+++ Assets/Systems/Guns/GunRay.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GunRay : MonoBehaviour
+{
+    //[Range(1f, 5000f)] [SerializeField] private float rayLength = 10f;
+    [SerializeField] private Transform leftGunMuzzle;
+    [SerializeField] private Transform rightGunMuzzle;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Gun Shot");
+            CastRayLeftGun();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right Gun Shot");
+            CastRayRightGun();
+        }
+    }
+
+    private void CastRayLeftGun()
+    {
+        RaycastHit hit;
+        Ray gunRay = new Ray(leftGunMuzzle.position, Vector3.forward);
+        
+        if (Physics.Raycast(gunRay, out hit))
+        {
+            if (hit.collider != null)
+            {
+                Debug.Log("Left gun hit " + hit.collider.gameObject.name);
+            }
+        }
+    }
+
+    private void CastRayRightGun()
+    {
+        RaycastHit hit;
+        Ray gunRay = new Ray(rightGunMuzzle.position, Vector3.forward);
+  
+        if (Physics.Raycast(gunRay, out hit))
+        {
+            if (hit.collider != null)
+            {
+                Debug.Log("Right gun hit " + hit.collider.gameObject.name);
+            }
+        }
+    }
+
+    
+}
