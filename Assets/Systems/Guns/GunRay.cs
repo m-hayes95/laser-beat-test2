68a915b7b71c44cf8ec88f291e6472f959545644
m-hayes95 <michaelhayes322@gmail.com>
2023-10-14 23:15:03 +0100

    Added gun animations and Gizmos

--- Assets/Systems/Guns/GunRay.cs
+++ Assets/Systems/Guns/GunRay.cs
@@ -8,17 +8,41 @@ public class GunRay : MonoBehaviour
     //[Range(1f, 5000f)] [SerializeField] private float rayLength = 10f;
     [SerializeField] private Transform leftGunMuzzle;
     [SerializeField] private Transform rightGunMuzzle;
+
+    [SerializeField] private GameObject leftGun;
+    [SerializeField] private GameObject rightGun;
+
+    private Animation gunShotLeftAnim;
+    private Animation gunShotRightAnim;
+
+    private AudioSource gunShotSound;
+
+    private Vector3 latestRayHitLeft;
+    private Vector3 latestRayHitRight;
+
+    private void Start()
+    {
+        gunShotLeftAnim = leftGun.GetComponent<Animation>();
+        gunShotRightAnim = rightGun.GetComponent<Animation>();
+        gunShotSound = leftGun.GetComponent<AudioSource>();    
+    }
     private void Update()
     {
+ 
+
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Left Gun Shot");
             CastRayLeftGun();
+            gunShotLeftAnim.Play();
+            gunShotSound.Play();
         }
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("Right Gun Shot");
             CastRayRightGun();
+            gunShotRightAnim.Play();
+            gunShotSound.Play();
         }
     }
 
@@ -31,6 +55,7 @@ public class GunRay : MonoBehaviour
         {
             if (hit.collider != null)
             {
+                latestRayHitLeft = hit.point;
                 Debug.Log("Left gun hit " + hit.collider.gameObject.name);
             }
         }
@@ -45,10 +70,24 @@ public class GunRay : MonoBehaviour
         {
             if (hit.collider != null)
             {
+                latestRayHitRight = hit.point;
                 Debug.Log("Right gun hit " + hit.collider.gameObject.name);
             }
         }
     }
 
-    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+  
+        if (latestRayHitLeft != null)
+        {
+            Gizmos.DrawLine(leftGunMuzzle.position, latestRayHitLeft);
+        }
+        if (latestRayHitRight != null)
+        {
+            Gizmos.DrawLine(rightGunMuzzle.position, latestRayHitRight);
+        }
+        
+    }
 }
